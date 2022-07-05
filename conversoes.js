25e7e7e1de96a13faf49987cbf71f86811a2c253
Brena Sarzi <sarzibrena@gmail.com>
2022-07-05 16:14:56 -0300

    erros

--- conversoes.js
+++ conversoes.js
@@ -16,4 +16,14 @@ const numeroString = "456";
 // aqui convertemos uma string por um número
 //console.log(numero + Number(numeroString))
 
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
 
+}
+console.log(respostaDeTudo, idade, pi)

cc3a6be3e2c1b3b3f358aa305d66836dbf9266c7
Brena Sarzi <sarzibrena@gmail.com>
2022-07-05 16:52:23 -0300

    ternarios

---
+++ ternario.js
@@ -0,0 +1,13 @@
+const idadeMinima = 18;
+const idadeCliente = 17;
+
+// if(idadeCliente >= idadeMinima){
+//     console.log("cerveja")
+// } else {
+//     console.log("suco")
+// }
+
+// Ternario:
+
+           //condição                     //true     //false
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")

56c1bd771ece8406e5fd239f1232877c85f2a1e8
Brena Sarzi <sarzibrena@gmail.com>
2022-07-05 17:02:49 -0300

    tamplate strings

---
+++ tamplate-strings.js
@@ -0,0 +1,11 @@
+const nome = 'Brena';
+const idade = 2021-2004;
+const cidadeDeNascimento = "São Paulo";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + ", nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é 
+${idade}, nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao)
+

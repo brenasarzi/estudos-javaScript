255759e1d192c5d78b6c9bf3fd456699342a2845
Brena Sarzi <sarzibrena@gmail.com>
2022-07-05 14:34:54 -0300

    conversões

---
+++ conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+const numero = 456;
+const numeroString = "456";
+
+//  console.log(numero === numeroString) -> false
+// console.log(numero == numeroString) -> true
+
+// console.log(numero + numeroString)
+
+
+// conversão explícita
+
+// aqui convertemos uma string por um número
+//console.log(numero + Number(numeroString))
+
+

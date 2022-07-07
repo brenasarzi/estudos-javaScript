69c215988dbc65fcace1f37a7312e1ad8d32bab7
Brena Sarzi <sarzibrena@gmail.com>
2022-07-07 16:39:39 -0300

    for

---
+++ for.js
@@ -0,0 +1,9 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+// i = valor de controle
+
+                    //6
+for (let i = 0; i < numeros.length; i++){
+                // numeros[0]
+    console.log(i, numeros[i])
+}
+

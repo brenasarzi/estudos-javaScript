b883d5d1009ca3273568e7e4573047c9934cc4e1
Brena Sarzi <sarzibrena@gmail.com>
2022-07-25 16:10:34 -0300

    inicio orientação de objeto

---
+++ orientacao-objeto/objeto-literal.js
@@ -0,0 +1,14 @@
+const user = {
+    nome: "Brena",
+    email: "sarzibrena@gmail.com",
+    nascimento: "16/09/2004",
+    role: "admin",
+    ativo: true,
+    exibirInfos: function(){
+        console.log(this.nome, this.email)
+    }
+}
+
+// herança de protótipo
+
+

52ee7dd26dbe56259f536a172e9c06a9d5440e17
Brena Sarzi <sarzibrena@gmail.com>
2022-07-25 16:53:09 -0300

    cadeia de protótipos objetos

--- orientacao-objeto/objeto-literal.js
+++ orientacao-objeto/objeto-literal.js
@@ -2,13 +2,38 @@ const user = {
     nome: "Brena",
     email: "sarzibrena@gmail.com",
     nascimento: "16/09/2004",
-    role: "admin",
+    role: "estudante",
     ativo: true,
     exibirInfos: function(){
         console.log(this.nome, this.email)
     }
 }
 
+const admin = {
+    nome: "Manuela",
+    email: "manuzinha@gmail.com",
+    role: "admin",
+    criarCurso(){
+        console.log("Curso criado com sucesso!")
+    }
+}
+// Objeto está usando um protótipo
+Object.setPrototypeOf(admin, user)
+
+admin.criarCurso()
+// Procura dentro do protótipo
+admin.exibirInfos()
+
+
 // herança de protótipo
+// user.exibirInfos()
 
+// // retornará undefined pois não encontrou a quem se refere
+// const exibir = function(){
+//     console.log(this.nome, this.email)
+// }
+// exibir() 
 
+// // liga uma função a um objeto, dando contexto a função
+// const exibirNome = exibir.bind(user)
+// exibirNome()

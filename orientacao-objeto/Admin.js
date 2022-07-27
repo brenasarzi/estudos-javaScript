ab708c1aa8af7e1408d95bda4a0471e438a271f4
Brena Sarzi <sarzibrena@gmail.com>
2022-07-27 15:28:48 -0300

    polimorfismo

--- orientacao-objeto/Admin.js
+++ orientacao-objeto/Admin.js
@@ -2,11 +2,17 @@ import User from "./User.js"
 
 // User é uma super classe de Admin, e Admin é uma sub clase de User
 export default class Admin extends User {
+    
     constructor(nome, email, nascimento, role = "admin", ativo = true){
         super(nome, email, nascimento, role, ativo)
     }
 
+    exibirInfos(){
+        return `${this.nome}, ${this.role}, ${this.ativo}`
+    }
+    
     criarCurso(nomeDoCurso, vagas){
         return `Curso de ${nomeDoCurso} criado com ${vagas} vagas`
     }
+
 }
